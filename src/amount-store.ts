4e57abb88800ef9c3302186c8466d8b3a76020ac
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-03-18 20:29:06 -0600

    add amountStore class with get, getKeyes, update, forEach, and getStore methods

--- src/amount-store.ts
+++ src/amount-store.ts
@@ -1,9 +1,32 @@
-import { Amount } from './amount';
+import { Map, List, Iterable, Iterator } from 'immutable';
+import { Amount, Metric } from './amount';
 
-class AmountStore {
-  constructor() {
+export interface AmountStore<K, V extends Metric> {
+  store: Map<K, Amount<V>>;
+}
 
+export class AmountStore<K, V extends Metric> implements AmountStore<K, V> {
+  constructor(store: Map<K, Amount<V>>) {
+    this.store = store;
   }
 
+  get(key: K):Amount<V> {
+    return this.store.get(key);
+  }
 
+  getKeys():Iterator<K> {
+    return this.store.keys();
+  }
+
+  update(key: K, newValue: Amount<V>):Map<K, Amount<V>> {
+    return this.store.update(key, value => newValue);
+  }
+
+  forEach(sideEffect: (value: Amount<V>, key: K, iter: Iterable<K, Amount<V>>) => any):number {
+    return this.store.forEach(sideEffect);
+  }
+
+  getStore() {
+    return this.store;
+  }
 }
